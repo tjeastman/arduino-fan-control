d5197db5b5051fca793baa32ab576ba264e36480
Thomas Eastman <tjeastman@gmail.com>
2015-05-30 13:18:39 -0700

    use a for loop to correctly implement print frequency

--- main.ino
+++ main.ino
@@ -6,7 +6,6 @@ int FAN_CONTROL_PIN = 11; // blue wire
 int FAN_POWER_PIN = 12;
 
 int OUTPUT_FREQUENCY = 1000;
-int iterations = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -19,32 +18,33 @@ void setup() {
 }
 
 void loop() {
-  iterations++;
+  int switch_state;
+  int value;
 
-  // turn power to the fan on or off via a MOSFET
-  int switch_state = digitalRead(FAN_SWITCH_PIN);
-  if (switch_state == LOW) {
-    digitalWrite(FAN_POWER_PIN, LOW);
-  } else {
-    digitalWrite(FAN_POWER_PIN, HIGH);
+  for (int i = 0; i < OUTPUT_FREQUENCY; ++i) {
+    // turn power to the fan on or off via a MOSFET
+    switch_state = digitalRead(FAN_SWITCH_PIN);
+    if (switch_state == LOW) {
+      digitalWrite(FAN_POWER_PIN, LOW);
+    } else {
+      digitalWrite(FAN_POWER_PIN, HIGH);
+    }
+
+    value = analogRead(INPUT_PIN);
+
+    // adjust the fan speed based on the input value
+    analogWrite(FAN_CONTROL_PIN, value / 4);
   }
 
-  int value = analogRead(INPUT_PIN);
+  // determine current RPM of the fan
+  // duration is in microseconds (1/1,000,000 of a second)
+  unsigned long pulse_duration = pulseIn(FAN_SENSE_PIN, LOW);
+  double frequency = 1000000 / pulse_duration;
 
-  // adjust the fan speed based on the input value
-  analogWrite(FAN_CONTROL_PIN, value / 4);
-
-  if (iterations % OUTPUT_FREQUENCY == 0) {
-    // determine current RPM of the fan
-    // duration is in microseconds (1/1,000,000 of a second)
-    unsigned long pulse_duration = pulseIn(FAN_SENSE_PIN, LOW);
-    double frequency = 1000000 / pulse_duration;
-
-    Serial.print("button state: ");
-    Serial.println(button);
-    Serial.print("input value: ");
-    Serial.println(value);
-    Serial.print("fan RPM:");
-    Serial.println(frequency / 2 * 60);
-  }
+  Serial.print("button state: ");
+  Serial.println(switch_state);
+  Serial.print("input value: ");
+  Serial.println(value);
+  Serial.print("fan RPM:");
+  Serial.println(frequency / 2 * 60);
 }
